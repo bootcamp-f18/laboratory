4277b33eb53734793e0630b2624f7ff94298c388
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2018-10-19 10:30:23 -0400

    Add laravel-mix, move sample component to its own file

---
+++ vue-components/Sample.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="btn" v-bind:class="colorclass" v-on:click="count++">
+        You clicked me {{ count }} time{{ count == 1 ? "" : "s" }}.
+    </button>
+</template>
+<script>
+    module.exports = {
+        props: ['colorclass'],
+
+        data: function () {
+
+            return {
+                count: 0
+            }
+        }
+    };
+</script>
--- vue-components/app.js
+++ vue-components/app.js
@@ -1,17 +1,4 @@
-Vue.component('sample', {
-
-    props: ['colorclass'],
-
-    data: function () {
-
-        return {
-            count: 0
-        }
-    },
-
-    template: '<button class="btn" v-bind:class="colorclass" v-on:click="count++">You clicked me {{ count }} time{{ count == 1 ? "" : "s" }}.</button>'
-
-});
+Vue.component('sample', require("./Sample.vue"));
 
 let app1 = new Vue({
 
